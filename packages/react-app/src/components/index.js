6e2aa087a20496a3587e168f49d7e228940daca5
ryann254 <keddelyronjoz@gmail.com>
2022-12-23 10:56:21 +0300

    feat: create AmountIn UI

--- packages/react-app/src/components/index.js
+++ packages/react-app/src/components/index.js
@@ -2,5 +2,8 @@
 import Loader from './Loader';
 import Exchange from './Exchange';
 import WalletButton from './WalletButton';
+import AmountIn from './AmountIn';
+import AmountOut from './AmountOut';
+import Balance from './Balance';
 
-export { Loader, Exchange, WalletButton };
+export { Loader, Exchange, WalletButton, AmountIn, AmountOut, Balance };
